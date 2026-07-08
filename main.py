1610f6c96f908e732abec316a5290c5cfceb2839
MaR1XyAnA <vlad.vlad.xxx1999@gmail.com>
2026-07-09 02:17:09 +0500

    222

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5 import QtWidgets
 from ui_manager import App
 
 # ВЕРСИЯ ПРИЛОЖЕНИЯ
-APP_VERSION = "1.1"
+APP_VERSION = "1.2"
 
 if __name__ == "__main__":
     q_app = QtWidgets.QApplication(sys.argv)
